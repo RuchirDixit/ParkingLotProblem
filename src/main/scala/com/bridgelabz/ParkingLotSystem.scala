4131b238cb8e6f1cea8ed27119652d21a5c63843
RuchirDixit <ruchirtd96@gmail.com>
2020-11-17 00:42:55 +0530

    [Ruchir]Refactored: Method to follow command and query principle

--- src/main/scala/com/bridgelabz/ParkingLotSystem.scala
+++ src/main/scala/com/bridgelabz/ParkingLotSystem.scala
@@ -2,18 +2,30 @@ package com.bridgelabz
 
 class ParkingLotSystem
 {
+
   var vehicle : Object = ""
   /**
    *
    * @param vehicle : Vehicle to park
    * @return : Boolean value if parked then true, else false
    */
-  def park(vehicle : Object): Boolean = {
-    if(this.vehicle != null) return false
-      this.vehicle = vehicle
-      true
+    @throws(classOf[ParkingLotException])
+  def park(vehicle : Object): Unit = {
+    if(this.vehicle != null){
+      throw new ParkingLotException("Parking Lot Full")
+    }
+    this.vehicle = vehicle
+  }
+  def isVehicleParked(vehicle: Object): Boolean = {
+    if(this.vehicle.equals(vehicle)) return true
+    false
   }
 
+  /**
+   *
+   * @param vehicle : Vehicle to unpark
+   * @return : return true if vehicle unparked, else false
+   */
   def unPark(vehicle : Object) : Boolean = {
     if(vehicle == null) return false
     if(this.vehicle.equals(vehicle))
