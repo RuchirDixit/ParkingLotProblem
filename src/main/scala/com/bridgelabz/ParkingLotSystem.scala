d8af6298178db5db8d0574dec4af2bde67dfd234
RuchirDixit <ruchirtd96@gmail.com>
2020-11-16 23:27:29 +0530

    [Ruchir]Added: Method and logic to check unpark cases

--- src/main/scala/com/bridgelabz/ParkingLotSystem.scala
+++ src/main/scala/com/bridgelabz/ParkingLotSystem.scala
@@ -2,13 +2,26 @@ package com.bridgelabz
 
 class ParkingLotSystem
 {
+  var vehicle : Object = ""
   /**
    *
    * @param vehicle : Vehicle to park
    * @return : Boolean value if parked then true, else false
    */
   def park(vehicle : Object): Boolean = {
-    true
+    if(this.vehicle != null) return false
+      this.vehicle = vehicle
+      true
+  }
+
+  def unPark(vehicle : Object) : Boolean = {
+    if(vehicle == null) return false
+    if(this.vehicle.equals(vehicle))
+    {
+      this.vehicle = null
+      return true
+    }
+    false
   }
 
 }
