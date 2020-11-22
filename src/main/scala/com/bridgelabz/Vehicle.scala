31a8d61a05a40d36167f7adcb0d20749ee1682cc
RuchirDixit <ruchirtd96@gmail.com>
2020-11-23 00:58:38 +0530

    [Added]: Time of park field in vehicle class

--- src/main/scala/com/bridgelabz/Vehicle.scala
+++ src/main/scala/com/bridgelabz/Vehicle.scala
@@ -1,7 +1,8 @@
 package com.bridgelabz
 
-class Vehicle(numberPlate:String,colour:String,brand:String) {
+class Vehicle(numberPlate:String,colour:String,brand:String,time:String*) {
   var numberPlateNumber = numberPlate
   var vehicleColour = colour
   var vehicleBrand = brand
+  var parkTime = time
 }
