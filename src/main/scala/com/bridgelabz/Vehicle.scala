be75e72d0cb5cbf5b2c2ce5bcfd56672a9021db8
RuchirDixit <ruchirtd96@gmail.com>
2020-11-23 00:03:52 +0530

    [Ruchir]Added: Vehicle class to support multiple parameters

---
+++ src/main/scala/com/bridgelabz/Vehicle.scala
@@ -0,0 +1,7 @@
+package com.bridgelabz
+
+class Vehicle(numberPlate:String,colour:String,brand:String) {
+  var numberPlateNumber = numberPlate
+  var vehicleColour = colour
+  var vehicleBrand = brand
+}
